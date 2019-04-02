e0124c3d5126107ea8f18069f53e280561281e36
Nuno Farinha <nadfarinha@gmail.com>
2019-04-02 18:15:26 +0100

    Create playground class

--- src/test/java/PlaygroundTest.java
+++ src/test/java/PlaygroundTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PlaygroundTest {
+
+    Playground playground1;
+
+    @Before
+    public void before() {
+        playground1 = new Playground("Sugar Land");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Sugar Land", playground1.getName());
+    }
 }
