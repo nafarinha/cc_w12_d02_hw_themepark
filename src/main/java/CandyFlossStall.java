583385dba48a29d6dd57eff19e58678bdd3ba83d
Nuno Farinha <nadfarinha@gmail.com>
2019-04-02 18:45:39 +0100

    Create candy floss stall class

--- src/main/java/CandyFlossStall.java
+++ src/main/java/CandyFlossStall.java
@@ -1,2 +1,8 @@
-public class CandyFlossStall {
+public class CandyFlossStall extends Stall {
+
+    public CandyFlossStall(String name, String ownerName, int parkingSpot) {
+        super(name, ownerName, parkingSpot);
+    }
 }
+
+
--- src/test/java/CandyFlossStallTest.java
+++ src/test/java/CandyFlossStallTest.java
@@ -1,2 +1,47 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CandyFlossStallTest {
+
+    CandyFlossStall candyFlossStall1;
+
+    @Before
+    public void before() {
+        candyFlossStall1 = new CandyFlossStall("Pink Cloud", "Janet Jameson", 30);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Pink Cloud", candyFlossStall1.getName());
+    }
+
+    @Test
+    public void canChangeName() {
+        candyFlossStall1.setName("Sugar Nimbus");
+        assertEquals("Sugar Nimbus", candyFlossStall1.getName());
+    }
+
+    @Test
+    public void hasOwnerName() {
+        assertEquals("Janet Jameson", candyFlossStall1.getOwnerName());
+    }
+
+    @Test
+    public void canChangeOwnerName() {
+        candyFlossStall1.setOwnerName("Francis Gordon");
+        assertEquals("Francis Gordon", candyFlossStall1.getOwnerName());
+    }
+
+    @Test
+    public void hasParkingSpot() {
+        assertEquals(30, candyFlossStall1.getParkingSpot());
+    }
+
+    @Test
+    public void canChangeParkingSpot() {
+        candyFlossStall1.setParkingSpot(33);
+        assertEquals(33, candyFlossStall1.getParkingSpot());
+    }
 }
