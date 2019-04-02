69f6adaf643220a1c77b2752108ef9e5b37397fc
Nuno Farinha <nadfarinha@gmail.com>
2019-04-02 21:35:51 +0100

    Refactor playground to use inherited methods

--- src/main/java/Playground.java
+++ src/main/java/Playground.java
@@ -1,18 +1,10 @@
 public class Playground extends Attraction implements ISecurity {
 
-    public Playground(String name, int rating) {
-        super(name, rating);
+    public Playground(String name) {
+        super(name);
     }
 
     public boolean isAllowedTo(Visitor newVisitor) {
         return newVisitor.getAge() < 16;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public int getRating() {
-        return this.rating;
     }
 }
