effde193719c3bc247517164d36918dc15bd6c03
Nuno Farinha <nadfarinha@gmail.com>
2019-04-02 21:16:02 +0100

    Refactor playground class to implement the inherited reviewed interface

--- src/main/java/Playground.java
+++ src/main/java/Playground.java
@@ -1,10 +1,18 @@
 public class Playground extends Attraction implements ISecurity {
 
-    public Playground(String name) {
-        super(name);
+    public Playground(String name, int rating) {
+        super(name, rating);
     }
 
     public boolean isAllowedTo(Visitor newVisitor) {
         return newVisitor.getAge() < 16;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getRating() {
+        return this.rating;
     }
 }
