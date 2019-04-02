54aa9821b944efe646756064c748e39916c2f3be
Nuno Farinha <nadfarinha@gmail.com>
2019-04-02 23:26:33 +0100

    Refactor theme park class constructor to accept varargs

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,15 +1,23 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class ThemePark {
 
     ArrayList<IReviewed> features;
 
-    public ThemePark() {
+    public ThemePark(IReviewed... args) {
         features = new ArrayList<>();
-
+        for(IReviewed arg : args) {
+            this.features.add(arg);
+        }
     }
 
     public ArrayList<IReviewed> getAllReviewed() {
        return this.features;
     }
+
+    public void addFeature(IReviewed newFeature) {
+        this.features.add(newFeature);
+    }
+
 }
--- src/test/java/ThemeParkTest.java
+++ src/test/java/ThemeParkTest.java
@@ -1,19 +1,49 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class ThemeParkTest {
+
+    //Attractions
+    Dodgems dodgems1;
+    Park park1;
+    Playground playground1;
+    Rollercoaster rollercoaster1;
+
+    //Stalls
+    CandyFlossStall candyFlossStall1;
+    IceCreamStall iceCreamStall1;
+    TobaccoStall tobaccoStall1;
 
     ThemePark themePark1;
 
     @Before
     public void before() {
-        themePark1 = new ThemePark();
+        dodgems1 = new Dodgems("Bumpy Drive");
+        park1 = new Park("Green Meadows");
+        playground1 = new Playground("Sugar Land");
+        rollercoaster1 = new Rollercoaster("Crazy Loops");
+
+        candyFlossStall1 = new CandyFlossStall("Pink Cloud", "Janet Jameson", 30);
+        iceCreamStall1 = new IceCreamStall("Vanilla Twist", "Jack Deer", 10);
+        tobaccoStall1 = new TobaccoStall("Cloudy Pipes", "Ewan Peterson", 86);
+
+
+        themePark1 = new ThemePark(dodgems1, park1, playground1, rollercoaster1, candyFlossStall1, iceCreamStall1, tobaccoStall1);
     }
+
 
     @Test
     public void canGetAllReviewedFeatures() {
-        assertEquals(0, themePark1.getAllReviewed().size());
+        assertEquals(7, themePark1.getAllReviewed().size());
     }
+
+ /*   @Test
+    public void canAddFeature() {
+
+    }*/
 }
